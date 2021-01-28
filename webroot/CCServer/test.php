f63427a4869e87d7e173825337f37ab257a1b205
System Administrator <root@jeremys-mbp.attlocal.net>
2021-01-28 14:33:46 -0500

    modified php x8 to print envt variables

--- webroot/CCServer/test.php
+++ webroot/CCServer/test.php
@@ -1,18 +1,22 @@
-<? php
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+  <body>
+
+ <? php
 
 	$hello = "OK";
 	$db_host = $_SERVER["MYSQL_SERVICE_HOST"];
 	$db_name = $_SERVER["DATABASE_NAME"];
 	$cc_database_user = $_SERVER["DATABASE_USER"];
 	$cc_database_password = $_SERVER["DB_SERVICE_PWD"];
+
+	echo $hello;
+
+	echo "<p>Hello Now</p>";
+
 ?>
 
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello Now</p>'; ?>
- <?php echo $hello; ?>
  </body>
 </html>
