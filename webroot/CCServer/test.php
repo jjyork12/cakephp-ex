fbda517eb005dd1359d3699393832227198582c4
System Administrator <root@jeremys-mbp.attlocal.net>
2021-01-28 14:10:19 -0500

    modified php x4 to print envt variables

--- webroot/CCServer/test.php
+++ webroot/CCServer/test.php
@@ -5,14 +5,17 @@
 	$cc_database_user = $_SERVER["DATABASE_USER"];
 	$cc_database_password = $_SERVER["DB_SERVICE_PWD"];
 
+	echo "<p>" . $db_host . "</p>";
+ 	echo "<p>" . $db_name . "</p>";
+?>
+
 <html>
  <head>
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?>
- <? php echo "<p>" . $db_host . "</p>"; ?>
- <? php echo "<p>" . $db_host . "</p>"; ?>
+ <?php echo '<p>Hello Now</p>'; ?>
  </body>
 </html>
 
+?>
