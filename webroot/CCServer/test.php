5def82a1babaca70f314e0f5ccca99c7a2dfb7ac
System Administrator <root@jeremys-mbp.attlocal.net>
2021-01-28 14:51:55 -0500

    modified php x11 to print envt variables

--- webroot/CCServer/test.php
+++ webroot/CCServer/test.php
@@ -7,13 +7,15 @@
 <?php
 
 	$hello = "OK";
-	$db_host = $_SERVER["MYSQL_SERVICE_HOST"];
-	$db_name = $_SERVER["DATABASE_NAME"];
-	$cc_database_user = $_SERVER["DATABASE_USER"];
-	$cc_database_password = $_SERVER["DB_SERVICE_PWD"];
+	$db_host = $_SERVER['MYSQL_SERVICE_HOST'];
+	$db_host2 = $_ENV['MYSQL_SERVICE_HOST'];
+	$db_name = $_SERVER['DATABASE_NAME'];
+	$cc_database_user = $_SERVER['DATABASE_USER'];
+	$cc_database_password = $_SERVER['DB_SERVICE_PWD'];
 
 	//echo $hello;
-	echo $db_host;
+	echo $hello;
+	echo $db_host2;
 
 	echo "<p>Hello Now</p>";
 
